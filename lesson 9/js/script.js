d16570620a6db7248f4d18b5579e1fbed9d58b8b
Kolpsi <tykataf@gmail.com>
2020-04-29 18:05:13 +0500

    lesson 12

--- lesson 9/js/script.js
+++ lesson 9/js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', function() {
         infoTab = infoHeader.querySelectorAll('.info-header-tab'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
-    function hideTabContent(a) {
+    let hideTabContent = (a) => {
         for (let  i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
     hideTabContent(1);
 
-    function showTabContent(b) {
+    let showTabContent = (b) => {
         for (let i = b; i < tabContent.length; i++) {
             if (tabContent[b].classList.contains('hide')) {
                 tabContent[b].classList.remove('hide');
@@ -38,9 +38,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // timer 
 
-    let deadline = "2020-04-01"
+    let deadline = "2020-05-4"
 
-    function getTimeRemaining(endtime) {
+      let getTimeRemaining = (endtime) => {
         let t = Date.parse(endtime) - Date.parse(new Date()),
         seconds = Math.floor((t / 1000) % 60),
         minutes = Math.floor((t / 1000 / 60) % 60),
@@ -54,7 +54,7 @@ window.addEventListener('DOMContentLoaded', function() {
         };
     }
 
-    function setClock(id, endtime) {
+    let setClock = (id, endtime) => {
         let timer = document.getElementById(id),
         hours = timer.querySelector('.hours'),
         minutes = timer.querySelector('.minutes'),
@@ -74,7 +74,7 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function pad(x) {
+    let pad = (x) => {
         return x < 10 ? '0' + x : x;
       }
 
